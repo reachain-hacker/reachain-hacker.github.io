1942b2939e18f4b4b1912b673870f327cdc83522
JimmyLv-ND <jimmy.jinglv@gmail.com>
2019-12-10 19:00:59 +0800

    add project

---
+++ precache-manifest.abe3e0d8bc383dbdbb23f6dce96f390a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74f0eeffdf2b62cb03e55569ef84cc59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a1c68d7bfda9d8a3008",
+    "url": "/static/css/2.1ade06be.chunk.css"
+  },
+  {
+    "revision": "03802eaed6a2bdd0b76f",
+    "url": "/static/css/main.667f9362.chunk.css"
+  },
+  {
+    "revision": "4a1c68d7bfda9d8a3008",
+    "url": "/static/js/2.4f1054c2.chunk.js"
+  },
+  {
+    "revision": "03802eaed6a2bdd0b76f",
+    "url": "/static/js/main.46406289.chunk.js"
+  },
+  {
+    "revision": "54f5701b9aa9817aa1a2",
+    "url": "/static/js/runtime-main.70accd27.js"
+  },
+  {
+    "revision": "f34c61b4ba7985b528d047c105e0f2bc",
+    "url": "/static/media/logo.f34c61b4.png"
+  }
+]);
